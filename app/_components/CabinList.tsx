9e723fa47936ae195f28f709fe2fe20e630bcf27
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-06-18 13:42:28 +0530

    add .env in gitignore

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -1,13 +1,34 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
 
-async function CabinList() {
+async function CabinList({ filter }: { filter: string }) {
   const cabins = await getCabins();
   if (!cabins.length) return null;
+  let displayCabins;
+  switch (filter) {
+    case "all": {
+      displayCabins = cabins;
+      break;
+    }
+    case "small": {
+      displayCabins = cabins.filter((c) => c.max_capacity <= 3);
+      break;
+    }
+    case "medium": {
+      displayCabins = cabins.filter(
+        (c) => c.max_capacity > 3 && c.max_capacity < 8
+      );
+      break;
+    }
+    case "large": {
+      displayCabins = cabins.filter((c) => c.max_capacity > 7);
+      break;
+    }
+  }
 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayCabins?.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
---
+++ app/_components/Filter.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+function Filter() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const activeFilter = searchParams.get("capacity") ?? "all";
+
+  function handleFilter(filter: string) {
+    const params = new URLSearchParams(searchParams);
+    params.set("capacity", filter);
+    const replacePath =
+      filter === "all" ? pathname : `${pathname}?${params.toString()}`;
+    router.replace(replacePath, {
+      scroll: false,
+    });
+  }
+  return (
+    <div className="border border-primary-800 flex">
+      <Button
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        All
+      </Button>
+      <Button
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        1&mdash;3 guests
+      </Button>
+      <Button
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        4&mdash;7 guests
+      </Button>
+      <Button
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        8&mdash;12 guests
+      </Button>
+    </div>
+  );
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      className={`${
+        filter === activeFilter && `bg-primary-700 text-primary-50`
+      } px-5 py-2 hover:bg-primary-700`}
+      onClick={() => handleFilter(filter)}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Filter;
